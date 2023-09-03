a24efa762c230041850ad4ba2a9faf8beb2019b6
Patel Swapnil <patelswapnil1004@gmail.com>
2023-09-03 21:44:56 +0530

    fix: server listen port

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,18 +4,23 @@ import { typeDefs } from "./schema";
 import { resolvers } from "./resolvers";
 import { TrackAPI } from "./datasources/track-api";
 
+
 async function startApolloServer() {
   const server = new ApolloServer({ typeDefs, resolvers });
   const { url } = await startStandaloneServer(server, {
     context: async () => {
-      const { cache } = server;
       return {
         dataSources: {
-          trackAPI: new TrackAPI({ cache }),
+          trackAPI: new TrackAPI(),
         },
       };
     },
-  });
+    listen :{
+      port:4000
+    }
+  }
+   
+  );
   console.log(`
     🚀  Server is running!
     📭  Query at ${url}
