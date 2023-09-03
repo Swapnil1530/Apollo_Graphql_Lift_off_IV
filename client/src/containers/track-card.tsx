4139a2a029d4bb9910930077a4957d83fbd21fe5
Patel Swapnil <patelswapnil1004@gmail.com>
2023-09-03 20:35:46 +0530

    feat : add Mutation in frontend ✨

--- client/src/containers/track-card.tsx
+++ client/src/containers/track-card.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useMutation } from "@apollo/client";
+import { gql } from "../__generated__";
 import styled from "@emotion/styled";
 import { colors, mq } from "../styles";
 import { humanReadableTimeFromSeconds } from "../utils/helpers";
@@ -9,11 +11,28 @@ import type { Track } from "../__generated__/graphql";
  * Track Card component renders basic info in a card format
  * for each track populating the tracks grid homepage.
  */
+const INCREMENT_TRACK_VIEWS = gql(`
+  mutation IncrementTrackViews($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      success
+      message
+      track {
+        id
+        numberOfViews
+      }
+    }
+  }
+`);
+
 const TrackCard: React.FC<{ track: Omit<Track, "modules"> }> = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+  });
 
   return (
-    <CardContainer to={`/track/${id}`}>
+    <CardContainer to={`/track/${id}`} onClick={() => incrementTrackViews()}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail || ""} alt={title} />
